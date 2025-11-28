122c4a0b65cb6dcf76963e3e6d0b002df4b1db66
Kubirub <165918473+Kubirub@users.noreply.github.com>
2025-11-28 15:52:20 +0700

    Add files via upload

--- grid_view.cpp
+++ grid_view.cpp
@@ -2,15 +2,13 @@
 #include <QPainter>
 #include <QMouseEvent>
 #include <QKeyEvent>
+#include <algorithm> 
 
-GridView::GridView(QWidget *parent)
-    : QWidget(parent)
-{
+GridView::GridView(QWidget *parent) : QWidget(parent) {
     setFocusPolicy(Qt::StrongFocus);
 }
 
-void GridView::paintEvent(QPaintEvent *)
-{
+void GridView::paintEvent(QPaintEvent *) {
     QPainter p(this);
     p.fillRect(rect(), Qt::white);
 
@@ -21,143 +19,132 @@ void GridView::paintEvent(QPaintEvent *)
         p.drawLine(0, y, width(), y);
 
     p.setPen(QPen(Qt::black, 2));
-    for (auto &route : m_routes)
-    {
-        for (int i = 0; i < route.size() - 1; i++)
-            p.drawLine(route[i], route[i+1]);
-    }   
+    for (auto &route : m_routes) {
+        for (int i = 0; i + 1 < static_cast<int>(route.path.size()); ++i)
+            p.drawLine(route.path[i], route.path[i + 1]);
+    }
 
-    for (int i = 0; i < m_points.size(); i++) {
-        if (i == m_selectedPoint)
-            p.setBrush(Qt::red);
-        else
-            p.setBrush(Qt::blue);
-
+    for (int i = 0; i < static_cast<int>(m_points.size()); i++) {
+        if (i == m_selectedPoint) p.setBrush(Qt::red);
+        else p.setBrush(Qt::blue);
         p.drawEllipse(m_points[i], 5, 5);
     }
 }
 
-int GridView::findPoint(const QPoint &pos)
-{
-    for (int i = 0; i < m_points.size(); i++) {
-        if (QLineF(pos, m_points[i]).length() <= 8)
-            return i;
+int GridView::findPoint(const QPoint &pos) {
+    for (int i = 0; i < static_cast<int>(m_points.size()); i++) {
+        if (QLineF(pos, m_points[i]).length() <= 8) return i;
     }
     return -1;
 }
 
-void GridView::mousePressEvent(QMouseEvent *e)
-{
+void GridView::mousePressEvent(QMouseEvent *e) {
     int idx = findPoint(e->pos());
-
     if (e->button() == Qt::LeftButton) {
-
         if (idx != -1) {
             if (m_selectedPoint == -1) {
                 m_selectedPoint = idx;
             } else if (m_selectedPoint != idx) {
-                m_routes.push_back(buildRoute(m_points[m_selectedPoint], m_points[idx]));
+
+                bool exists = false;
+                for (const auto &route : m_routes) {
+                    if ((route.a == m_selectedPoint && route.b == idx) ||
+                        (route.a == idx && route.b == m_selectedPoint)) {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists) {
+                    Route r;
+                    r.a = m_selectedPoint;
+                    r.b = idx;
+                    r.path = buildRoute(m_points[r.a], m_points[r.b]);
+                    m_routes.push_back(std::move(r));
+                }
+
                 m_selectedPoint = -1;
             }
-
             m_dragPoint = idx;
             m_isDragging = true;
         } else {
             m_points.push_back(snapToGrid(e->pos()));
-            m_selectedPoint = m_points.size() - 1;
+            m_selectedPoint = (int)m_points.size() - 1;
         }
     }
-
     update();
 }
 
-void GridView::mouseMoveEvent(QMouseEvent *e)
-{
+
+void GridView::mouseMoveEvent(QMouseEvent *e) {
     if (m_isDragging && m_dragPoint != -1) {
-
         QPoint oldPos = m_points[m_dragPoint];
-
         QPoint newPos = snapToGrid(e->pos());
+        if (newPos == oldPos) return; 
+
         m_points[m_dragPoint] = newPos;
 
-        for (auto &route : m_routes)
-        {
-            QPoint &start = route.front();
-            QPoint &end   = route.back();
-
+        for (auto &route : m_routes) {
             bool updated = false;
-
-            if (start == oldPos) {
-                start = newPos;
-                updated = true;
+            if (route.a == m_dragPoint || route.b == m_dragPoint) {
+                if (route.a >= 0 && route.a < static_cast<int>(m_points.size()) &&
+                    route.b >= 0 && route.b < static_cast<int>(m_points.size())) {
+                    route.path = buildRoute(m_points[route.a], m_points[route.b]);
+                    updated = true;
+                }
             }
-
-            if (end == oldPos) {
-                end = newPos;
-                updated = true;
-            }
-
-            if (updated) {
-                route = buildRoute(start, end);
-            }
+            (void)updated; 
         }
-
         update();
     }
 }
 
-
-void GridView::mouseReleaseEvent(QMouseEvent *)
-{
+void GridView::mouseReleaseEvent(QMouseEvent *) {
     m_isDragging = false;
     m_dragPoint = -1;
 }
 
-void GridView::keyPressEvent(QKeyEvent *e)
-{
+void GridView::keyPressEvent(QKeyEvent *e) {
     if (e->key() == Qt::Key_Delete) {
-
         if (m_selectedPoint != -1) {
-
-            QPoint removedPoint = m_points[m_selectedPoint];
+            int removedIdx = m_selectedPoint;
 
             m_routes.erase(
                 std::remove_if(
-                    m_routes.begin(),
-                    m_routes.end(),
-                    [&](const auto &r) {
-                        return r.front() == removedPoint || r.back() == removedPoint;
-                    }
+                    m_routes.begin(), m_routes.end(),
+                    [&](const Route &r) { return r.a == removedIdx || r.b == removedIdx; }
                 ),
                 m_routes.end()
             );
 
-            m_points.erase(m_points.begin() + m_selectedPoint);
+            m_points.erase(m_points.begin() + removedIdx);
+
+            for (auto &route : m_routes) {
+                if (route.a > removedIdx) --route.a;
+                if (route.b > removedIdx) --route.b;
+                if (route.a >= 0 && route.a < static_cast<int>(m_points.size()) &&
+                    route.b >= 0 && route.b < static_cast<int>(m_points.size())) {
+                    route.path = buildRoute(m_points[route.a], m_points[route.b]);
+                }
+            }
 
             m_selectedPoint = -1;
             update();
         }
-
     }
 }
 
-QPoint GridView::snapToGrid(const QPoint &p)
-{
+QPoint GridView::snapToGrid(const QPoint &p) {
     int x = (p.x() + m_cellSize / 2) / m_cellSize * m_cellSize;
     int y = (p.y() + m_cellSize / 2) / m_cellSize * m_cellSize;
     return QPoint(x, y);
 }
 
-std::vector<QPoint> GridView::buildRoute(const QPoint &a, const QPoint &b)
-{
+std::vector<QPoint> GridView::buildRoute(const QPoint &a, const QPoint &b) {
     std::vector<QPoint> route;
-
     route.push_back(a);
-
     QPoint mid(b.x(), a.y());
     route.push_back(mid);
-
     route.push_back(b);
-
     return route;
 }
--- grid_view.h
+++ grid_view.h
@@ -5,10 +5,8 @@
 #include <QPoint>
 #include <vector>
 
-class GridView : public QWidget
-{
+class GridView : public QWidget {
     Q_OBJECT
-
 public:
     explicit GridView(QWidget *parent = nullptr);
 
@@ -18,17 +16,21 @@ protected:
     void mouseMoveEvent(QMouseEvent *) override;
     void mouseReleaseEvent(QMouseEvent *) override;
     void keyPressEvent(QKeyEvent *) override;
-    QPoint snapToGrid(const QPoint &p);
 
+    QPoint snapToGrid(const QPoint &p);
 
 private:
     int m_cellSize = 25;
-
     std::vector<QPoint> m_points;
-    std::vector<std::vector<QPoint>> m_routes;
+
+    struct Route {
+        int a = -1;
+        int b = -1;
+        std::vector<QPoint> path;
+    };
+    std::vector<Route> m_routes;
+
     std::vector<QPoint> buildRoute(const QPoint &a, const QPoint &b);
-
-
 
     int m_selectedPoint = -1;
     int m_dragPoint = -1;
